cf6b0a4bae88b88c164c280559826c9f4c23741a
lyfwmcn <1562764778@qq.com>
2026-07-08 18:39:54 +0800

    添加 EXReg, MReg

---
+++ src/EXReg.v
@@ -0,0 +1,67 @@
+module EXReg (
+    input CLK,
+    input RST,
+    input EN,
+    input CLR,
+    input ZF,
+    input CF,
+    input SF,
+    input OF,
+    input RegWr,
+    input [1:0] PCCtr,
+    input [1:0] RegSrc,
+    input [2:0] BranchCtr,
+    input [4:0] Rd,
+    input [31:0] imm,
+    input [31:0] PC,
+    input [31:0] nextPC,
+    input [31:0] BusW,
+    output reg _ZF,
+    output reg _CF,
+    output reg _SF,
+    output reg _OF,
+    output reg _RegWr,
+    output reg [1:0] _PCCtr,
+    output reg [1:0] _RegSrc,
+    output reg [2:0] _BranchCtr,
+    output reg [4:0] _Rd,
+    output reg [31:0] _imm,
+    output reg [31:0] _PC,
+    output reg [31:0] _nextPC,
+    output reg [31:0] _BusW
+);
+
+always @(posedge CLK or posedge RST) begin
+    if (RST == 1'h1) begin
+        _ZF <= 1'h1;
+        _CF <= 1'h0;
+        _SF <= 1'h0;
+        _OF <= 1'h0;
+        _RegWr <= 1'h1;
+        _PCCtr <= 2'h0;
+        _RegSrc <= 2'h0;
+        _BranchCtr <= 3'h2;
+        _Rd <= 5'h0;
+        _imm <= 32'h0;
+        _PC <= 32'h0;
+        _nextPC <= 32'h4;
+        _BusW <= 32'h0;
+    end
+    else if (EN == 1'h1) begin
+        _ZF <= CLR == 1'h1 ? 1'h1 : ZF;
+        _CF <= CLR == 1'h1 ? 1'h0 : CF;
+        _SF <= CLR == 1'h1 ? 1'h0 : SF;
+        _OF <= CLR == 1'h1 ? 1'h0 : OF;
+        _RegWr <= CLR == 1'h1 ? 1'h1 : RegWr;
+        _PCCtr <= CLR == 1'h1 ? 2'h0 : PCCtr;
+        _RegSrc <= CLR == 1'h1 ? 2'h0 : RegSrc;
+        _BranchCtr <= CLR == 1'h1 ? 3'h2 : BranchCtr;
+        _Rd <= CLR == 1'h1 ? 5'h0 : Rd;
+        _imm <= CLR == 1'h1 ? 32'h0 : imm;
+        _PC <= CLR == 1'h1 ? 32'h0 : PC;
+        _nextPC <= CLR == 1'h1 ? 32'h4 : nextPC;
+        _BusW <= CLR == 1'h1 ? 32'h0 : BusW;
+    end
+end
+
+endmodule
---
+++ src/MReg.v
@@ -0,0 +1,43 @@
+module MReg (
+    input CLK,
+    input RST,
+    input EN,
+    input CLR,
+    input RegWr,
+    input [1:0] RegSrc,
+    input [4:0] Rd,
+    input [31:0] imm,
+    input [31:0] nextPC,
+    input [31:0] BusW,
+    input [31:0] mem,
+    output reg _RegWr,
+    output reg [1:0] _RegSrc,
+    output reg [4:0] _Rd,
+    output reg [31:0] _imm,
+    output reg [31:0] _nextPC,
+    output reg [31:0] _BusW,
+    output reg [31:0] _mem
+);
+
+always @(posedge CLK or posedge RST) begin
+    if (RST == 1'h1) begin
+        _RegWr <= 1'h1;
+        _RegSrc <= 2'h0;
+        _Rd <= 5'h0;
+        _imm <= 32'h0;
+        _nextPC <= 32'h4;
+        _BusW <= 32'h0;
+        _mem <= 32'h0;
+    end
+    else if (EN == 1'h1) begin
+        _RegWr <= CLR == 1'h1 ? 1'h1 : RegWr;
+        _RegSrc <= CLR == 1'h1 ? 2'h0 : RegSrc;
+        _Rd <= CLR == 1'h1 ? 5'h0 : Rd;
+        _imm <= CLR == 1'h1 ? 32'h0 : imm;
+        _nextPC <= CLR == 1'h1 ? 32'h4 : nextPC;
+        _BusW <= CLR == 1'h1 ? 32'h0 : BusW;
+        _mem <= CLR == 1'h1 ? 32'h0 : mem;
+    end
+end
+
+endmodule
